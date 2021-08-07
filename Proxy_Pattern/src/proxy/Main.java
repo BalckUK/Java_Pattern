31456b328ae7b1716dbcc0af5a1e47913b9f152f
UMK <haga5032@naver.com>
2021-08-07 10:13:19 +0900

    proxy패턴 완성

--- Proxy_Pattern/src/proxy/Main.java
+++ Proxy_Pattern/src/proxy/Main.java
@@ -3,10 +3,17 @@ package proxy;
 public class Main {
 	public static void main(String[] args) {
 		Browser browser = new Browser("www.naver.com");
+		/*browser.show();
 		browser.show();
 		browser.show();
 		browser.show();
-		browser.show();
-		browser.show();
+		browser.show();*/
+		
+		IBrower browser1 = new BrowserProxy("www.naver.com");
+		browser1.show();
+		browser1.show();
+		browser1.show();
+		browser1.show();
+		browser1.show();
 	}
 }
